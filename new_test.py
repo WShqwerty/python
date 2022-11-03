31e57d533b220cbee45928c9576e6feb436a4de0
WShqwerty <3168923454@qq.com>
2022-11-03 22:18:01 +0800

    commit all

--- new_test.py
+++ new_test.py
@@ -1,4 +1,19 @@
-import math
+from selenium import webdriver
+from time import sleep
 
-print(math.pi)
+def main():
+    print("hello")
+    url = "https://www.google.com"
+    try:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        sleep(2)
+
+    finally:
+        driver.close()
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
 
