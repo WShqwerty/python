63c7e194e576a97c6b1d1850af337d1e67f9f563
WShqwerty <3168923454@qq.com>
2022-11-04 19:52:02 +0800

    commit all

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from urllib import request
 
 
 print("hello")
@@ -8,10 +9,20 @@ print("do test commit!")
 # vscode使用git，需要commit 然后push就可以上传到github
  
 def math_add(n,m):
-  return n+m+n*m*m;
+  return n+m+n*m*m
+
+
+def url_work():
+  print("start work!")
+  url = "https://www.top250.com"
+
+  data = request.urlopen(url)
+  print(data.read())
+
  
 if __name__ == "__main__":
   print(math_add(1,2))
   print("hello")
 
   print("hello!")
+  url_work()
